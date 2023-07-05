d98bd6cfd17056512a497103450cf913b30108e2
1234 <arpitsinghthakur321@gmail.com>
2023-07-06 01:26:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<b>Welcome to react 18, Here we will import all components of react</b>
+    </div>
+  );
+}
+
+export default App;
